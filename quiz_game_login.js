f9ad399a7588d81f16a3fc3d9aac2f30e955c3fe
picachu16 <89108160+picachu16@users.noreply.github.com>
2021-11-26 15:51:44 -0800

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,7 +1,7 @@
 function getuser(){
 player1=document.getElementById("player1_input").value;
 player2=document.getElementById("player2_input").value;
-localStorage.setItem("player1", player1);
-localStorage.setItem("player2", player2);
+localStorage.setItem("player1_name", player1);
+localStorage.setItem("player2_name", player2);
 window.location="game_page.html";
 }
